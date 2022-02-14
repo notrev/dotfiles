b4a9a33071abc1d3b04fc9ac89ca5d6647d09fb8
Éverton Arruda <root@earruda.eti.br>
2022-02-14 16:31:00 +0100

    Add python installer

---
+++ installers/python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+#################
+### Variables ###
+#################
+
+# none
+
+########################
+### Installation ###
+########################
+
+# Update PIP version
+echo ""
+echo "### Updating pip version for python3"
+python3 -m pip install --upgrade pip
