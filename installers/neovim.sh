487750a241865a296eaf2e424b2b16ce74fda6b0
Éverton Arruda <root@earruda.eti.br>
2022-02-14 17:13:54 +0100

    Update NEOVIM installer
    - Copy neovim config files to ~/.config/nvim directory
    - Install plugins and their dependencies

--- installers/neovim.sh
+++ installers/neovim.sh
@@ -4,16 +4,16 @@
 ### Variables ###
 #################
 
-# none
+VIM_UNDOFILES_DIR=$HOME/.vim-undo-files
 
 ########################
 ### Installation ###
 ########################
 
 # Install neovim
-# echo ""
-# echo "### Installing neovim"
-# brew install neovim
+echo ""
+echo "### Installing neovim"
+brew install neovim
 
 # Install neovim's python 2 and 3 providers 
 echo ""
@@ -23,5 +23,24 @@ python3 -m pip install neovim
 echo ""
 echo "### Installing neovim's node provider"
 npm install -g neovim
+
+# Create directories
+echo ""
+echo "### Creating directories"
+mkdir -p $VIM_UNDOFILES_DIR
+
+# Copy settings files
+echo ""
+echo "### Copying settings"
+cp -r .config/nvim ~/.config/
+
+echo ""
+echo "### Installing plugin dependencies"
+brew install fzf
+
+# Start nvim to install plugins
+echo ""
+echo "### Starting nvim to install plugins"
+nvim
 
 echo "Done!"
